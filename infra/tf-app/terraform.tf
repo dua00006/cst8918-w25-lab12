cf55b2c13c42add6d61e7238d02378e24a8936c6
sing1249 <sing1249@algonquinlive.com>
2025-03-30 19:38:17 -0400

    chore: Fix Step 2 issues

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "sing1249-githubactions-rg"
-    storage_account_name = "sing1249githubactions1" #Updated the name as we Accidentally did in wrong subscription and name can not be used again.
+    storage_account_name = "sing1249githubactions1"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
   }
