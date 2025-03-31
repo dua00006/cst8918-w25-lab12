9afa88aae44bfb5c1f5404ea44f57a2fd7dc09c2
sing1249 <sing1249@algonquinlive.com>
2025-03-31 18:29:14 -0400

    Add Virtual Network and Subnet for infra-elements

---
+++ infra/tf-app/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "sing1249-a12-rg"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "Canada Central"
+}
