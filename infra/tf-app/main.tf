3315cf52390de3363216816fef70b9501b71e781
sing1249 <sing1249@algonquinlive.com>
2025-03-31 19:10:21 -0400

    Test artifact pass for terraform apply

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -16,4 +16,8 @@ resource "azurerm_subnet" "main_subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  tags = {
+    owner = "sing1249"
+  }
 }
